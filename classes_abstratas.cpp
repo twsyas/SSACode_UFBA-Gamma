444f66e3932727079b1053f3cc608da511c6b299
yas <106776203+twsyas@users.noreply.github.com>
2023-08-30 14:52:02 -0300

    abstract class in cpp

---
+++ classes_abstratas.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+class Polygon {
+    protected:
+        int width, height;
+    public:
+        void set_values (int a, int b)
+            { width = a; height = b; }
+        virtual int area()=0;
+        void printArea()
+            { cout << this -> area() << '\n';}
+        
+};
+
+class Retangle: public Polygon{
+    public:
+        int area (void)
+            {return (width*height);}
+};
+
+class Triangle: public Polygon {
+    public:
+        int area(void)
+            {return (width*height/2);}
+};
+
+int main(){
+    Retangle rect;
+    Triangle trgl;
+    Polygon * ppoly1 = &rect;
+    Polygon * ppoly2 = &trgl;
+    ppoly1 -> set_values (4,5);
+    ppoly2 -> set_values (4,5);
+    ppoly1 -> printArea();
+    ppoly2 -> printArea();
+    return 0;
+    
+}
